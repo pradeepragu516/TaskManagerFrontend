4858bc23502d4b99d18aaa488cbe8fea314666b2
pradeepragu516 <pradeep.m2023it@sece.ac.in>
2025-01-31 11:38:49 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+export const server = "helpful-taiyaki-c7c7c1.netlify.app"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
